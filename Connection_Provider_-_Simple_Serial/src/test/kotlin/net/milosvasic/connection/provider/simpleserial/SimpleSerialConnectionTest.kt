871e39eb56ce03f99e10f7697322e346f383deb8
Milos Vasic <milos85vasic@gmail.com>
2018-01-13 17:25:50 +0100

    Connection Provider - Working on simple serial connection provider.

---
+++ Connection_Provider_-_Simple_Serial/src/test/kotlin/net/milosvasic/connection/provider/simpleserial/SimpleSerialConnectionTest.kt
@@ -0,0 +1,12 @@
+package net.milosvasic.connection.provider.simpleserial
+
+import org.junit.Test
+
+public class SimpleSerialConnectionTest {
+
+    @Test
+    fun testSimpleSerialConnection(){
+
+    }
+
+}
