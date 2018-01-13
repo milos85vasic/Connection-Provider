29fed76b03bb342b71e29840d0a163540591949d
Milos Vasic <milos85vasic@gmail.com>
2018-01-13 19:06:48 +0100

    Connection Provider - Writing simple test.

--- Connection_Provider_-_Simple_Serial/src/test/kotlin/net/milosvasic/connection/provider/simpleserial/SimpleSerialConnectionTest.kt
+++ Connection_Provider_-_Simple_Serial/src/test/kotlin/net/milosvasic/connection/provider/simpleserial/SimpleSerialConnectionTest.kt
@@ -15,9 +15,38 @@ public class SimpleSerialConnectionTest {
 
     @Test
     fun testSimpleSerialConnection() {
-        val criteria = SimpleSerialConnectionProvidingCriteria("", callback, "")
+        val criteria = SimpleSerialConnectionProvidingCriteria(
+                "/dev/stdin",
+                callback,
+                "/dev/stdout"
+        )
         val connection = ConnectionProvider.provide(criteria)
         Assert.assertNotNull(connection)
+        // Connecting - Disconnecting in a row.
+        for (x in 0..10) {
+            try {
+                connection.connect()
+            } catch (e: Exception) {
+                fail(e)
+            }
+            // Confirm we can't connect twice.
+            var failed = false
+            try {
+                connection.connect()
+            } catch (e: Exception) {
+                failed = true
+            }
+            Assert.assertTrue(failed)
+            try {
+                connection.disconnect()
+            } catch (e: Exception) {
+                fail(e)
+            }
+        }
+    }
+
+    private fun fail(e: Exception) {
+        Assert.fail("Error: $e")
     }
 
 }
