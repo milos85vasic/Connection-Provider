c54befebad05814d98dd543a711758594133bec9
Milos Vasic <milos85vasic@gmail.com>
2018-01-13 15:12:26 +0100

    Connection Provider - Dependencies added.

---
+++ Connection_Provider_-_Simple_Serial/src/main/kotlin/simple_serial/net.milosvasic.connection.provider/SimpleSerialConnectionProvidingCriteria.kt
@@ -0,0 +1,9 @@
+package simple_serial.net.milosvasic.connection.provider
+
+import provider.net.milosvasic.connection.ConnectionProvidingCriteria
+
+public class SimpleSerialConnectionProvidingCriteria : ConnectionProvidingCriteria {
+
+
+
+}
