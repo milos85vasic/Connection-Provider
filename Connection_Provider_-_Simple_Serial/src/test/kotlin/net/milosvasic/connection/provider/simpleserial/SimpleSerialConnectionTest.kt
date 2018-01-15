c934923a0b47653f126fc44447bcd815423498b5
Milos Vasic <milos85vasic@gmail.com>
2018-01-15 15:27:51 +0100

    Connection Provider - Working on main mechanism.

--- Connection_Provider_-_Simple_Serial/src/test/kotlin/net/milosvasic/connection/provider/simpleserial/SimpleSerialConnectionTest.kt
+++ Connection_Provider_-_Simple_Serial/src/test/kotlin/net/milosvasic/connection/provider/simpleserial/SimpleSerialConnectionTest.kt
@@ -2,6 +2,7 @@ package net.milosvasic.connection.provider.simpleserial
 
 import net.milosvasic.connection.provider.commons.ConnectionCallback
 import net.milosvasic.connection.provider.commons.ConnectionProvider
+import net.milosvasic.logger.ConsoleLogger
 import net.milosvasic.testing.toolkit.ToolkitTest
 import org.junit.Assert
 import java.io.File
@@ -9,19 +10,26 @@ import java.io.File
 class SimpleSerialConnectionTest : ToolkitTest() {
 
     private var toWrite = ""
+    private val logger = ConsoleLogger()
+    private val tag = javaClass.simpleName
     private val path = "${System.getProperty("user.home")}/test.txt"
 
     private val callback = object : ConnectionCallback {
         override fun onConnectivityChanged(connected: Boolean) {
+            log("Connected: $connected")
             unlock()
         }
 
         override fun onDataReceived(data: ByteArray) {
-            // TODO: Implement this.
+            val received = String(data)
+            log("Received: $received")
+            unlock()
         }
 
         override fun onDataWritten(data: ByteArray) {
-            Assert.assertTrue(data.toString() == toWrite)
+            val written = String(data)
+            log("Written: $written (to write was: $toWrite)")
+            Assert.assertTrue(written == toWrite)
             unlock()
         }
 
@@ -43,26 +51,25 @@ class SimpleSerialConnectionTest : ToolkitTest() {
         val criteria = SimpleSerialConnectionProvidingCriteria(path, callback)
         val connection = ConnectionProvider.provide(criteria)
         Assert.assertNotNull(connection)
-        // Connecting - Disconnecting in a row.
+        // Connecting - Disconnecting N times in a row
         for (x in 0..10) {
+            // Connect
             connection.connect()
             lock()
-
             // Confirm we are connected.
             Assert.assertTrue(connection.isConnected())
-
             // Confirm we can't connect twice.
             // connection.connect() // TODO: Will fail!
-
-            // Try to write data N times:
+            // lock()
+            // Writing data N times in a row
             for (y in 0..10) {
-                connection.write("$y\n".toByteArray())
+                toWrite = "$y"
+                connection.write(toWrite.toByteArray())
                 lock()
             }
-
+            // Disconnect
             connection.disconnect()
             lock()
-
             Assert.assertFalse(connection.isConnected())
         }
     }
@@ -74,5 +81,9 @@ class SimpleSerialConnectionTest : ToolkitTest() {
     private fun fail(error: String) {
         Assert.fail(error)
     }
+
+    private fun log(msg: String) = logger.v(tag, msg)
+
+    private fun err(msg: String) = logger.e(tag, msg)
 
 }
